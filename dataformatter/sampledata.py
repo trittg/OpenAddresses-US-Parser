ef028ef02bc561ec160a632918a6c3f0eb555c46
Tobie <tobias.rittgers@my.ccsu.edu>
2019-04-18 11:57:30 -0400

    Sample now formats zipcodes
    added zipcode formmating

--- dataformatter/sampledata.py
+++ dataformatter/sampledata.py
@@ -3,8 +3,25 @@ import numpy as np
 
 def format_csv(inpath, outpath, num):
     #If the csv is larger than 500 mb this program dies, very sad
-    data = pd.read_csv(inpath)
-
+    data = pd.read_csv(inpath, dtype=object)
     sample = data.sample(n=num, random_state=1)
+    sample.reset_index(inplace=True)
+
+    for i in range(len(sample['POSTCODE'])):
+        zipcode = str(sample['POSTCODE'][i])
+        # case zipcode contains - character, then select all earlier chracters
+        hypenpos = zipcode.find('-')
+        if hypenpos != -1:
+            zipcode = zipcode[:hypenpos]
+        # case zipcode contains illegal chracter, then drop the value
+        if not zipcode.isdigit():
+            zipcode = np.nan
+        # case zipcode is larger than 5 digits (assume it has the 4 digit routing code)
+        # only take the characters before the 4 digit routing number
+        elif len(zipcode) > 5:
+            zipcode = zipcode[:-4]
+        sample['POSTCODE'][i] = zipcode
+
+    del sample['index']
     sample.to_csv(outpath, index=False)
     return
