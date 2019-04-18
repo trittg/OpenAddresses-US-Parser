1805b590d66f7b8ebcc988c0def04bc64549655e
Tobie <tobias.rittgers@my.ccsu.edu>
2019-04-18 09:52:47 -0400

    Test zipcode format
    added string based zipcode format

--- dataformatter/testcityfilter.py
+++ dataformatter/testcityfilter.py
@@ -4,18 +4,23 @@ import numpy as np
 import os.path
 
 inpath = 'dataformatter\\testcity.csv'
-data = pd.read_csv(inpath, dtype=str)
+newdata = pd.read_csv(inpath, dtype=str)
 
-print(data)
+print(newdata)
 
-data['POSTCODE'].where(data['POSTCODE'].str.isdigit(),
-                       other=(data['POSTCODE'].str.split('-')), inplace=True)
-
-
-
-print(data)
-
-mystr = "Hello world!"
-mylist = mystr.split('-')
-print(mylist)
-print(mylist[0])
+print(len(newdata['POSTCODE']))
+for i in range(len(newdata['POSTCODE'])):
+    zipcode = str(newdata['POSTCODE'][i])
+    # case zipcode contains - character, then select all earlier chracters
+    hypenpos = zipcode.find('-')
+    if hypenpos != -1:
+        zipcode = zipcode[:hypenpos]
+    # case zipcode contains illegal chracter, then drop the value
+    if not zipcode.isdigit():
+        zipcode = np.nan
+    # case zipcode is larger than 5 digits (assume it has the 4 digit routing code)
+    # only take the characters before the 4 digit routing number
+    elif len(zipcode) > 5:
+        zipcode = zipcode[:-4]
+    newdata['POSTCODE'][i] = zipcode
+print(newdata)
